f985306d3dbfb12f3db08791f10678ba3ea7b7ca
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-01-21 08:41:08 +0700

    Use next

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => <div>hello world</div>;
-const root = document.getElementById('root');
-
-ReactDOM.render(<App />, root);
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Index = () => <div>aa</div>;
+
+export default Index;
