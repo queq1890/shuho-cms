22fb073b2742beb69409b53649a2792ac831422b
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-01-21 13:51:55 +0700

    Can create a PR

--- src/pages/api/github/pull.ts
+++ src/pages/api/github/pull.ts
@@ -1,32 +1,106 @@
 import { NextApiHandler } from 'next';
 import { Octokit } from '@octokit/rest';
 
+type Option = {
+  owner: string;
+  repo: string;
+  baseBranch: string;
+  newBranch: string;
+  pullRequest: {
+    title: string;
+    body: string;
+  };
+  commitMessage?: string;
+};
+
 const octokit = new Octokit({
   auth: process.env.SHUHO_TOKEN,
 });
+
+const main = async ({
+  owner,
+  repo,
+  baseBranch,
+  newBranch,
+  pullRequest: { body, title },
+  commitMessage = 'WIP',
+}: Option) => {
+  const baseBranchRef = await octokit.git.getRef({
+    owner,
+    repo,
+    ref: `heads/${baseBranch}`,
+  });
+
+  const newBranchRef = await octokit.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${newBranch}`,
+    sha: baseBranchRef.data.object.sha,
+  });
+
+  const currentCommit = await octokit.git.getCommit({
+    owner,
+    repo,
+    commit_sha: newBranchRef.data.object.sha,
+  });
+
+  const newCommit = await octokit.git.createCommit({
+    owner,
+    repo,
+    message: commitMessage,
+    tree: currentCommit.data.tree.sha,
+    parents: [currentCommit.data.sha],
+  });
+
+  await octokit.git.updateRef({
+    owner,
+    repo,
+    ref: `heads/${newBranch}`,
+    sha: newCommit.data.sha,
+  });
+
+  await octokit.pulls.create({
+    owner,
+    repo,
+    head: newBranch,
+    base: baseBranch,
+    title,
+    body,
+  });
+};
+
+const getOption = ({ newBranch }: { newBranch: string }) => {
+  const option: Option = {
+    owner: 'queq1890',
+    repo: 'octokit-playground',
+    // repo: 'shuho',
+    baseBranch: 'master',
+    newBranch,
+    pullRequest: {
+      title: newBranch,
+      body: 'This PR is generated with shuho-cms.',
+    },
+  };
+
+  return option;
+};
 
 const pullHandler: NextApiHandler = async (req, res) => {
   const { method } = req;
 
   switch (method) {
-    case 'GET': {
-      const { data } = await octokit.pulls.get({
-        owner: 'queq1890',
-        repo: 'shuho',
-        pull_number: 5,
-        mediaType: {
-          format: 'diff',
-        },
-      });
+    case 'POST': {
+      const option = getOption({ newBranch: 'foo' });
+      await main(option);
 
-      res.status(200).json({ data });
+      res.status(200).json({ ok: true });
       break;
     }
 
     // Get data from your database
 
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import useSWR from 'swr';
+// import useSWR from 'swr';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Index = () => {
-  const { data } = useSWR('/api/github/pull', fetcher);
+  // const { data } = useSWR('/api/github/pull', fetcher);
 
-  return <div>{data && <pre>{JSON.stringify(data, null, '\n')}</pre>}</div>;
+  return <div>aa</div>;
 };
-
-<div>aa</div>;
 
 export default Index;
