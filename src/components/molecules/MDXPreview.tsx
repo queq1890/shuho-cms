6567cdf46a5f7729d5beceafe6ea96a6ede03621
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-01-28 17:13:47 +0700

    Can preview markdown

--- src/components/molecules/MDXPreview.tsx
+++ src/components/molecules/MDXPreview.tsx
@@ -1,5 +1,20 @@
-const MDXPreview = () => {
-  return <div>a</div>;
+import styled from '@emotion/styled';
+import { usePreviewContext } from 'contexts/previewContext';
+import { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MDXPreview: FC = () => {
+  const previewText = usePreviewContext();
+
+  return (
+    <Root>
+      <ReactMarkdown>{previewText}</ReactMarkdown>
+    </Root>
+  );
 };
+
+const Root = styled('div')`
+  height: 100%;
+`;
 
 export default MDXPreview;
